b0be2f84e502cec0b6fb0d0177eadbab344a48b3
pankaj03032006 <pankaj03032006@gmail.com>
2026-04-26 12:06:55 +0530

    setup superuser

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,14 +1,6 @@
 from django.apps import AppConfig
 
+
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
-
-    def ready(self):
-        import os
-        if os.environ.get('RUN_MAIN') != 'true':
-            return
-
-        from django.contrib.auth.models import User
-        if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser('admin', 'admin@gmail.com', 'admin123')
